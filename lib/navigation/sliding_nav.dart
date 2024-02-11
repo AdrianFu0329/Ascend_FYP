b7c0bc2499599328b644137b7da334f7e27a82b6
Adrian <adrianfwl0329@gmail.com>
2024-02-12 00:37:08 +0800

    Changes to be committed: 	modified:   lib/navigation/sliding_nav.dart

--- lib/navigation/sliding_nav.dart
+++ lib/navigation/sliding_nav.dart
@@ -31,18 +31,7 @@ class SlidingNav<T> extends PageRoute<T> {
         begin: const Offset(1.0, 0.0),
         end: Offset.zero,
       ).animate(animation),
-      child: ScaleTransition(
-        scale: Tween<double>(
-          begin: 0.8,
-          end: 1.0,
-        ).animate(
-          CurvedAnimation(
-            parent: animation,
-            curve: Curves.easeInOut,
-          ),
-        ),
-        child: child,
-      ),
+      child: child,
     );
   }
 }
