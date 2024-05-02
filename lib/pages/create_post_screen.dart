27c6e2d5d9bba703597e7e021dabc6075b1f7c6f
Adrian <adrianfwl0329@gmail.com>
2024-05-02 11:42:34 +0800

    Development: Create post image picker

--- lib/pages/create_post_screen.dart
+++ lib/pages/create_post_screen.dart
@@ -1,7 +1,8 @@
-import 'package:ascend_fyp/models/media.dart';
-import 'package:ascend_fyp/pages/picker_screen.dart';
+import 'dart:io';
+
 import 'package:ascend_fyp/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePostScreen extends StatefulWidget {
   const CreatePostScreen({
@@ -13,90 +14,56 @@ class CreatePostScreen extends StatefulWidget {
 }
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
-  final List<Media> _selectedMedias = [];
-
-  Future<void> obtainMedia() async {
-    final List<Media>? result = await Navigator.push<List<Media>>(
-      context,
-      MaterialPageRoute(
-        builder: (context) => PickerScreen(selectedMedia: _selectedMedias),
-      ),
-    );
-
-    if (result != null) {
-      _updateSelectedMedias(result);
-    }
-  }
-
-  void _updateSelectedMedias(List<Media> result) {
-    setState(() {
-      _selectedMedias.clear();
-      _selectedMedias.addAll(result);
-    });
-  }
-
-  Widget displayMedia(List<Media> selectedMedia) {
-    if (selectedMedia.isEmpty) {
-      return GestureDetector(
-        onTap: obtainMedia,
-        child: Container(
-          width: 100,
-          height: 100,
-          color: Colors.grey,
-          child: const Center(
-            child: Icon(
-              Icons.add,
-              size: 40,
-              color: Colors.red,
-            ),
-          ),
-        ),
-      );
-    } else {
-      return ListView.builder(
-        itemCount: selectedMedia.length + 1,
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          if (index == selectedMedia.length) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: obtainMedia,
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.grey,
-                  child: const Center(
-                    child: Icon(
-                      Icons.add,
-                      size: 40,
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
-              ),
-            );
-          } else {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: const BoxDecoration(
-                  color: Colors.grey,
-                ),
-                child: selectedMedia[index].widget,
-              ),
-            );
-          }
-        },
-      );
-    }
-  }
+  final List<File> _images = [];
 
   @override
   Widget build(BuildContext context) {
+    final picker = ImagePicker();
+
+    void _showMessage(String message) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            content: Text(
+              message,
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'OK',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    Future<void> getImage() async {
+      final pickedImages = await picker.pickMultiImage();
+
+      setState(() {
+        if (pickedImages != null) {
+          int remainingSlots = 10 - _images.length;
+          _images.addAll(pickedImages
+              .take(remainingSlots)
+              .map((pickedImage) => File(pickedImage.path)));
+          if (pickedImages.length > remainingSlots) {
+            _showMessage("You can only select up to 10 media items.");
+          }
+        } else {
+          debugPrint("No images picked");
+        }
+      });
+    }
+
     TextEditingController titleController = TextEditingController();
     TextEditingController descriptionController = TextEditingController();
     TextEditingController locationController = TextEditingController();
@@ -128,41 +95,95 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         title: Text(
           'Create Post',
-          style: Theme.of(context).textTheme.titleLarge,
+          style: Theme.of(context).textTheme.titleLarge!,
         ),
         actions: [
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
             child: ElevatedButton(
               onPressed: () {},
-              child: const Text('Create'),
               style: buttonStyle,
+              child: const Text('Create'),
             ),
           ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            displayMedia(_selectedMedias),
-            const SizedBox(height: 24.0),
-            CustomTextField(
-              controller: titleController,
-              hintText: "Title",
-            ),
-            const SizedBox(height: 24.0),
-            CustomTextField(
-              controller: descriptionController,
-              hintText: "Description",
-            ),
-            const SizedBox(height: 24.0),
-            CustomTextField(
-              controller: locationController,
-              hintText: "Location",
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Display selected images
+              if (_images.isNotEmpty) ...[
+                SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _images.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.file(
+                          _images[index],
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                // Add button to select more images
+                GestureDetector(
+                  onTap: getImage,
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    child: const Center(
+                      child: Icon(
+                        Icons.add,
+                        size: 40,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+              ] else ...[
+                GestureDetector(
+                  onTap: getImage,
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    child: const Center(
+                      child: Icon(
+                        Icons.add,
+                        size: 40,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+              const SizedBox(height: 24.0),
+              CustomTextField(
+                controller: titleController,
+                hintText: "Title",
+              ),
+              const SizedBox(height: 24.0),
+              CustomTextField(
+                controller: descriptionController,
+                hintText: "Description",
+              ),
+              const SizedBox(height: 24.0),
+              CustomTextField(
+                controller: locationController,
+                hintText: "Location",
+              ),
+            ],
+          ),
         ),
       ),
     );
