cd19a1022ea5dcd96198a2e829d41a6a5f744a97
Adrian <adrianfwl0329@gmail.com>
2024-06-08 12:27:38 +0800

    Dev: Media post loading animation at home screen

--- lib/social media/screens/home_screen.dart
+++ lib/social media/screens/home_screen.dart
@@ -29,6 +29,31 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Widget loadingCard() {
+    return SizedBox(
+      width: 135,
+      height: 250,
+      child: Card(
+        elevation: 4.0,
+        color: Theme.of(context).cardColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 175,
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: const Center(
+                child: ContainerLoadingAnimation(),
+              ),
+            ),
+            const SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     void modalBottomSheet(Widget screen) {
@@ -106,7 +131,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         builder: (context, snapshot) {
                           if (snapshot.connectionState ==
                               ConnectionState.waiting) {
-                            return const CustomLoadingAnimation();
+                            return loadingCard();
                           } else if (snapshot.hasError) {
                             return const Center(
                               child: Text(
