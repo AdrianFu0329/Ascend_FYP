378e288882cbbaef203bba20f235917c2e2b7b7b
Adrian <adrianfwl0329@gmail.com>
2024-06-19 11:19:42 +0800

    Fix: Moved loading animation to the bottom

--- lib/social media/screens/home_screen.dart
+++ lib/social media/screens/home_screen.dart
@@ -25,7 +25,6 @@ class _HomeScreenState extends State<HomeScreen> {
   final currentUser = FirebaseAuth.instance.currentUser!;
   StreamSubscription<QuerySnapshot>? notificationsSubscription;
   bool hasNewNotifications = false;
-  //Map<String, Map<String, dynamic>> postList = {};
   Map<String, dynamic> postList = {};
   bool isLoading = true;
 
@@ -62,20 +61,22 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> fetchPostsFromDatabase() async {
+    setState(() {
+      isLoading = true;
+    });
+
     DatabaseReference postsRef = FirebaseDatabase.instance.ref('posts');
     DatabaseEvent event = await postsRef.orderByChild('timestamp').once();
 
     Map<String, dynamic> loadedPosts = {};
 
     if (event.snapshot.value != null) {
-      // Convert data into a Map<String, dynamic>
       Map<dynamic, dynamic> postsData =
           event.snapshot.value as Map<dynamic, dynamic>;
       postsData.forEach((key, value) {
         loadedPosts[key.toString()] = Map<String, dynamic>.from(value as Map);
       });
 
-      // Sort the posts by timestamp in descending order
       List<MapEntry<String, dynamic>> sortedPosts =
           loadedPosts.entries.toList();
       sortedPosts
@@ -84,7 +85,6 @@ class _HomeScreenState extends State<HomeScreen> {
       loadedPosts = {for (var entry in sortedPosts) entry.key: entry.value};
     }
 
-    // Preload media for each post
     List<Future<dynamic>> futures = [];
     for (var postId in loadedPosts.keys) {
       var post = loadedPosts[postId]!;
@@ -101,10 +101,8 @@ class _HomeScreenState extends State<HomeScreen> {
       }
     }
 
-    // Wait for all media to be loaded
     await Future.wait(futures);
 
-    // Update state with loaded posts and set loading state to false
     setState(() {
       postList = loadedPosts;
       isLoading = false;
@@ -136,7 +134,6 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> refreshPosts() async {
-    // Reset state and reload posts
     setState(() {
       postList = {};
       isLoading = true;
@@ -158,110 +155,115 @@ class _HomeScreenState extends State<HomeScreen> {
     return RefreshIndicator(
       onRefresh: refreshPosts,
       child: Scaffold(
-        body: isLoading
-            ? const Center(child: ContainerLoadingAnimation())
-            : CustomScrollView(
-                slivers: [
-                  SliverAppBar(
-                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                    title: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 36, 0, 36),
-                      child: Image.asset(
-                        "lib/assets/images/logo_noBg.png",
-                        width: 130,
-                        height: 50,
-                      ),
-                    ),
-                    actions: [
-                      Stack(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              modalBottomSheet(const NotificationModal());
-                            },
-                            icon: const Icon(Icons.notifications),
-                            color: Colors.white,
-                            iconSize: 24,
-                          ),
-                          if (hasNewNotifications)
-                            Positioned(
-                              right: 11,
-                              top: 11,
-                              child: Container(
-                                padding: const EdgeInsets.all(2),
-                                decoration: const BoxDecoration(
-                                  color: Colors.red,
-                                  shape: BoxShape.circle,
-                                ),
-                                constraints: const BoxConstraints(
-                                  minWidth: 8,
-                                  minHeight: 8,
-                                ),
-                              ),
-                            ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  postList.isEmpty
-                      ? const SliverToBoxAdapter(
-                          child: Center(child: Text("No posts available")))
-                      : SliverMasonryGrid.count(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 8,
-                          crossAxisSpacing: 8,
-                          itemBuilder: (BuildContext context, int index) {
-                            String postId = postList.keys.elementAt(index);
-                            Map<String, dynamic> data = postList[postId]!;
-
-                            String title = data['title'];
-                            List<ImageWithDimension> images =
-                                data['images'] ?? [];
-                            VideoPlayerController? videoController =
-                                data['videoController'];
-                            List<String> likes =
-                                List<String>.from(data['likes'] ?? []);
-                            String userId = data['userId'];
-                            int timestamp = data['timestamp'];
-                            String description = data['description'];
-                            String location = data['location'];
-                            String type = data['type'];
-
-                            return type == "Images"
-                                ? MediaCard(
-                                    index: index,
-                                    postId: postId,
-                                    media: images,
-                                    title: title,
-                                    userId: userId,
-                                    likes: likes,
-                                    timestamp:
-                                        Timestamp.fromMillisecondsSinceEpoch(
-                                            timestamp),
-                                    description: description,
-                                    location: location,
-                                    type: type,
-                                  )
-                                : MediaCard(
-                                    index: index,
-                                    postId: postId,
-                                    media: videoController,
-                                    title: title,
-                                    userId: userId,
-                                    likes: likes,
-                                    timestamp:
-                                        Timestamp.fromMillisecondsSinceEpoch(
-                                            timestamp),
-                                    description: description,
-                                    location: location,
-                                    type: type,
-                                  );
-                          },
-                          childCount: postList.length,
-                        ),
-                  const SliverToBoxAdapter(child: SizedBox(height: 24)),
-                ],
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+              title: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 36, 0, 36),
+                child: Image.asset(
+                  "lib/assets/images/logo_noBg.png",
+                  width: 130,
+                  height: 50,
+                ),
               ),
+              actions: [
+                Stack(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        modalBottomSheet(const NotificationModal());
+                      },
+                      icon: const Icon(Icons.notifications),
+                      color: Colors.white,
+                      iconSize: 24,
+                    ),
+                    if (hasNewNotifications)
+                      Positioned(
+                        right: 11,
+                        top: 11,
+                        child: Container(
+                          padding: const EdgeInsets.all(2),
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                          constraints: const BoxConstraints(
+                            minWidth: 8,
+                            minHeight: 8,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ],
+            ),
+            if (isLoading)
+              SliverFillRemaining(
+                child: Center(
+                  child: ContainerLoadingAnimation(),
+                ),
+              )
+            else if (postList.isEmpty)
+              SliverToBoxAdapter(
+                child: Center(child: Text("No posts available")),
+              )
+            else
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                sliver: SliverMasonryGrid.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  itemBuilder: (BuildContext context, int index) {
+                    String postId = postList.keys.elementAt(index);
+                    Map<String, dynamic> data = postList[postId]!;
+
+                    String title = data['title'];
+                    List<ImageWithDimension> images = data['images'] ?? [];
+                    VideoPlayerController? videoController =
+                        data['videoController'];
+                    List<String> likes = List<String>.from(data['likes'] ?? []);
+                    String userId = data['userId'];
+                    int timestamp = data['timestamp'];
+                    String description = data['description'];
+                    String location = data['location'];
+                    String type = data['type'];
+
+                    return type == "Images"
+                        ? MediaCard(
+                            index: index,
+                            postId: postId,
+                            media: images,
+                            title: title,
+                            userId: userId,
+                            likes: likes,
+                            timestamp:
+                                Timestamp.fromMillisecondsSinceEpoch(timestamp),
+                            description: description,
+                            location: location,
+                            type: type,
+                          )
+                        : MediaCard(
+                            index: index,
+                            postId: postId,
+                            media: videoController,
+                            title: title,
+                            userId: userId,
+                            likes: likes,
+                            timestamp:
+                                Timestamp.fromMillisecondsSinceEpoch(timestamp),
+                            description: description,
+                            location: location,
+                            type: type,
+                          );
+                  },
+                  childCount: postList.length,
+                ),
+              ),
+            const SliverToBoxAdapter(child: SizedBox(height: 24)),
+          ],
+        ),
       ),
     );
   }
